373e2824fa84e99458aaca434b003bcef8fbf1fc
Anji-Gujjula <117161450+Anji-Gujjula@users.noreply.github.com>
2024-08-22 10:17:51 +0530

    Update FileUpload.js

--- FileUpload.js
+++ FileUpload.js
@@ -1,11 +1,24 @@
-// src/FileUpload.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000'); // Connect to the WebSocket server
+const socket = io('http://localhost:4000');
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to WebSocket server');
+    });
+
+    socket.on('connect_error', (err) => {
+      console.error('Connection error:', err);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from WebSocket server');
+    });
+  }, []);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -17,6 +30,7 @@ const FileUpload = () => {
       reader.onload = function (e) {
         const fileData = e.target.result;
         socket.emit('file-upload', { name: file.name, data: fileData });
+        console.log('File uploaded:', file.name);
       };
       reader.readAsDataURL(file);
     }
